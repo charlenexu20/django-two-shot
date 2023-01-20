ce176f45fb3a0ebb0d23f612ab8aac6bc1213c88
charlene <charlene.xu92@gmail.com>
2023-01-20 11:30:12 -0800

    Feature 14 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -5,10 +5,12 @@ from receipts.views import (
     category_list,
     account_list,
     create_category,
+    create_account,
 )
 
 
 urlpatterns = [
+    path("accounts/create/", create_account, name="create_account"),
     path("categories/create/", create_category, name="create_category"),
     path("accounts/", account_list, name="account_list"),
     path("categories/", category_list, name="category_list"),
